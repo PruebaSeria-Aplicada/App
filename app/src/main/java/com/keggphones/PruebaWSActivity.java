f0e34c307a6626ba82f79ef97c40d8dc6ba099b4
Michael <michael.melendezm@gmail.com>
2016-10-18 09:29:52 -0600

    consumiendo WS BCCR

--- app/src/main/java/com/keggphones/PruebaWSActivity.java
+++ app/src/main/java/com/keggphones/PruebaWSActivity.java
@@ -2,19 +2,22 @@ package com.keggphones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.keggphones.WS.ClientWS;
 
 public class PruebaWSActivity extends AppCompatActivity {
+
+    public static  TextView txtWS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_ws);
 
+        txtWS = (TextView)findViewById(R.id.txtMessageWS);
 
-
-        new ClientWS(this).execute("michael","msdhsg1212");
+        new ClientWS(this).execute("317","16/10/16","18/10/16","Gerardo","no");
 
     }
 }
